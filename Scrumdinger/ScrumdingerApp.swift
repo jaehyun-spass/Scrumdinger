10042d6383bea672737fb4caef120c92570119ea
jaehyun-spass <reacreate@spass.ai>
2023-01-08 16:59:42 +0900

    first commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by avenj on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
